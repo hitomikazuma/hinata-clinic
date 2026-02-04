453d1e73fc1ce12653d6ab6978bb87c42db64c53
hitomikazuma <hitomi@hitomikazumanoMacBook-Pro.local>
2026-02-04 12:36:12 +0900

    s

--- wp/footer.php
+++ wp/footer.php
@@ -1,12 +1,112 @@
 <footer class="footer">
   <div class="inner">
-    <div class="footerWrap">
-      <div class="footerContent">
-        <p class="footerTitle">
-          <img src="<?php echo esc_url(get_theme_file_uri('images/logo_white.png')); ?>" width="200" height="200" alt="logo">
+    <div>
+      <p class="footerTitle">
+        この街の、家族の健康と
+        <br>
+        あなたの輝きを支えたい
+      </p>
+      <ul class="footerSnsList">
+        <li class="footerSnsItem">
+          <a href="#">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/footer/icon_instagram.png')); ?>" width="22" height="22" alt="Instagram">
+          </a>
+        </li>
+        <li class="footerSnsItem">
+          <a href="#">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/footer/icon_x.png')); ?>" width="20" height="20" alt="x">
+          </a>
+        </li>
+        <li class="footerSnsItem">
+          <a href="#">
+            <img src="<?php echo esc_url(get_theme_file_uri('images/footer/icon_line.png')); ?>" width="23" height="23" alt="line">
+          </a>
+        </li>
+      </ul>
+      <p class="footerCopyright">
+        <small>
+          Copyright &copy; 2026 Hinata Clinic. All rights reserved.
+        </small>
+      </p>
+    </div>
+    <div class="footerNav">
+      <ul class="footerNavList">
+        <li class="footerNavItem">
+          <a href="#">当院について</a>
+        </li>
+        <li class="footerNavItem">
+          <a href="#">診療案内</a>
+          <ul class="footerNavSubList">
+            <li class="footerNavSubItem">
+              <a href="#">一般内科</a>
+            </li>
+            <li class="footerNavSubItem">
+              <a href="#">小児科</a>
+            </li>
+            <li class="footerNavSubItem">
+              <a href="#">外科・整形外科</a>
+            </li>
+            <li class="footerNavSubItem">
+              <a href="#">在宅医療</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="footerNavList">
+        <li class="footerNavItem">
+          <a href="#">予防医療</a>
+          <ul class="footerNavSubList">
+            <li class="footerNavSubItem">
+              <a href="#">予防接種・ワクチン</a>
+            </li>
+            <li class="footerNavSubItem">
+              <a href="#">健康診断</a>
+            </li>
+            <li class="footerNavSubItem">
+              <a href="#">美容医療</a>
+            </li>
+          </ul>
+        </li>
+        <li class="footerNavItem">
+          <a href="#">診療時間・アクセス</a>
+        </li>
+        <li class="footerNavItem">
+          <a href="#">ボランティア募集</a>
+        </li>
+        <li class="footerNavItem">
+          <a href="#">お知らせ</a>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <img src="<?php echo esc_url(get_theme_file_uri('images/logo.png')); ?>" width="216" height="82" alt="Hinata Clinic">
+      <div class="phone">
+        <p class="phoneNumber">
+          <img src="<?php echo esc_url(get_theme_file_uri('images/icon_contact.png')); ?>" alt="Phone">
+          0997-69-4622
         </p>
       </div>
+      <div>
+        <p class="footerTextPrimary"><span>診療時間</span> 9:30-12:00 / 14:00-18:00</p>
+        <p class="footerTextPrimary"><span>休診日</span> 土日祝日</p>
+        <p class="footerText">FAX：0997-69-5518</p>
+        <p class="footerText">〒894-0027 鹿児島県奄美市名瀬末広町2-27<br>陽向ビル2F</p>
+      </div>
     </div>
+    <p class="goTop">
+      <a href="#">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          width="25px" height="25px">
+          <path fill-rule="evenodd" fill="rgb(11, 158, 206)"
+            d="M0.162,12.162 C0.162,5.535 5.535,0.162 12.162,0.162 C18.790,0.162 24.162,5.535 24.162,12.162 C24.162,18.790 18.790,24.162 12.162,24.162 C5.535,24.162 0.162,18.790 0.162,12.162 Z" />
+          <path fill-rule="evenodd" stroke-width="2px" stroke="rgb(255, 255, 255)" fill="rgb(255, 255, 255)"
+            d="M12.003,9.002 L10.010,13.996 L13.996,13.996 L12.003,9.002 Z" />
+        </svg>
+        Page top
+      </a>
+    </p>
   </div>
 </footer>
 
